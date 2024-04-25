76629a2d911a94ede62acd6aa1e32876e4f46b41
Daniel Bisina <danbis664@gmail.com>
2024-04-25 02:28:28 +0000

    commit

--- scraper.js
+++ scraper.js
@@ -9,7 +9,6 @@ async function scrapeCoindesk() {
   let browser;
   try {
     browser = await puppeteer.connect({ browserWSEndpoint: process.env.BROWSER_WS_ENDPOINT });
-    browser = await puppeteer.launch();
     const [page] = await browser.pages();
     const ua =
       "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36";
